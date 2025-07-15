1b45e36241406d12eb6718e8780195e3cfd27f22
theDNWA <dnwa906090@gmail.com>
2025-07-16 00:41:32 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,10 +2,14 @@ import math
 
 def calculate_factorial():
     try:
-        n = int(input("Введите положительное целое число: "))
+        n = int(input("Введите положительное целое число:"))
         if n < 0:
-            raise ValueError("Число не может быть отрицательным")
+            raise ValueError("Не может быть отрицательным")
         result = math.factorial(n)
         print(f"Факториал числа {n} равен: {result}")
-    except ValueError as e:
-        print(f"Ошибка: {str(e) or 'Введены некорректные данные. Требуется положительное целое число.'}")
+    except ValueError:
+        print(f"Ошибка: { 'Некорректные данные, введите положительное целое число.' }")
+
+calculate_factorial()
+
+input("Готово, нажмите любую клавишу, чтобы выйти")
